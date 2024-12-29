b144b14cf4d4867c4795c7fe2a1014ba85d494d4
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-12-30 01:27:22 +0600

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -36,13 +37,45 @@ android {
 }
 
 dependencies {
-
+    // AndroidX Core Libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.activity)
+
+    // Testing Libraries
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Architectural Components (Lifecycle & ViewModel)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Room Database
+    implementation(libs.androidx.room.runtime.v252)
+    ksp(libs.androidx.room.compiler.v252)
+    implementation(libs.androidx.room.ktx.v252)
+
+    // Kotlin Coroutines
+    implementation(libs.kotlinx.coroutines.core.v173)
+    implementation(libs.kotlinx.coroutines.android.v173)
+
+    // Retrofit (Networking)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor.v4110)
+
+    // Navigation Components
+    implementation(libs.androidx.navigation.fragment.ktx.v274)
+    implementation(libs.androidx.navigation.ui.ktx.v274)
+
+    // Glide (Image Loading)
+    implementation(libs.glide.v4151)
+    ksp(libs.compiler.v4151)
+
+    // Optional - Dependency Injection (Dagger/Hilt)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
